fd8c3536edf7c3649ade0a4c45975352a4613de4
Ethan Freeman <ethanfree23@gmail.com>
2021-11-11 14:23:01 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(name, event){
+    let newArray = [];
+    for (let i=0; i<name.length;i++){
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    };
+    return newArray;
+};
+  
+function countDown(num) {
+    let loop = num+1;
+    while (loop > 0) {
+        console.log(--loop);
+        
+    } 
+};
